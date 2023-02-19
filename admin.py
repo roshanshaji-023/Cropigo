9d2502d781387760ee98f425fedbf4e23f38852f
roshan <roshanshaji023@gmail.com>
2023-02-19 11:53:33 +0530

    admin panel

--- admin.py
+++ admin.py
@@ -3,12 +3,18 @@ from database import *
 
 admin=Blueprint('admin',__name__) #creating blueprint for admin page
 
+
 @admin.route('/admin',methods=['get','post'])
 def adminhome():
-    if 'product' in request.form:
-        name=request.form['productname']
-        type=request.form['type']
-        print(name,type)
+    if 'user-search' in request.form:
+        name=request.form['user-search-name']
+        type='user'
+        q="select * from user_login where user_name='%s' and user_type='%s'"%(name,type)
+        if name=='all':
+            q="select * from user_login"
+        res=select(q)
+        print(res)
+        return render_template('adminhome.html',data=res)
     return render_template('adminhome.html')
 
 
