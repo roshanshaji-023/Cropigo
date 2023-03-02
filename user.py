010c43365afc1bf47f02e9e377b74c9c23e345b9
roshan shaji <roshanshaji023@gmail.com>
2023-03-02 18:19:33 +0530

    implemented ml to the model for crop prediction

--- user.py
+++ user.py
@@ -1,18 +1,44 @@
 from flask import Flask,Blueprint,render_template,request, session,url_for,redirect
 from datetime import date
 from database import *
+# Importing libraries
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report
+from sklearn import metrics
+from sklearn import tree
+import warnings
+warnings.filterwarnings('ignore')
+
 user=Blueprint('user',__name__) #creating blueprint for user dashboard page@public.route('/userdashboard')
-@user.route('/userdashboard')
+@user.route('/userdashboard',methods=['get','post'])
 def userdashboard():
     if 'crop-predict' in request.form:
         nitrogen=request.form['nitrogen']
         phosphorus=request.form['phosphorus']
         potassium=request.form['potassium']
-        temperature=request.form['temperatur']
+        temperature=request.form['temperature']
         humidity=request.form['humidity']
         ph=request.form['ph']
         
         
+        # Dump the trained Naive Bayes classifier with Pickle
+        RF_pkl_filename = 'static\RandomForest.pkl'
+        # Open the file to load pkl file
+
+        with  open(RF_pkl_filename, 'rb') as RF_Model_pkl:
+            RF=pickle.load(RF_Model_pkl)
+        
+        data = np.array([[104,18, 30, 23.603016, 60.3, 6.7, 140.91]])
+        prediction = RF.predict(data)
+        print(prediction)
+        result="The best to cultivate predicted is %s"%prediction
+        print(result)
+        return render_template('userdashboard.html',data=result)
     return render_template('userdashboard.html')
 
 @user.route('/usercomplaints',methods=['get','post'])
